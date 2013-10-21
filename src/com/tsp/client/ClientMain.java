7065b3faaa487fd7509e32fa92da6e289b08780d
Tyler Schenk <schenk.tyler@gmail.com>
2013-10-20 23:16:50 -0400

    testing first commit

--- src/com/tsp/client/ClientMain.java
+++ src/com/tsp/client/ClientMain.java
@@ -2,18 +2,24 @@ package com.tsp.client;
 
 import com.tsp.client.controller.ConnectionController;
 import com.tsp.client.controller.GameController;
+import com.tsp.client.controller.StartupController;
 import com.tsp.client.model.GameModel;
 import com.tsp.client.view.GameView;
+import com.tsp.client.view.StartupView;
 
 
 public class ClientMain {
 	public static void main(String[] arguments) {
 		
+		
+		StartupController sc = new StartupController();
+		StartupView sv = new StartupView(sc);
+		sv.setVisible(true);
+		
 		GameModel gm = new GameModel();
 		GameView gv = new GameView(gm);
 		GameController gc = new GameController();
 		ConnectionController cc = new ConnectionController();
-		
 		gv.addListener(gc);
 		gv.addListener(cc);
 		gv.play();
